197de3d59205b180ed278f9148609d631775f402
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-09-02 18:46:28 +0530

    Add script to update files

---
+++ src/main/assets/update.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+SERVER="http://scrollback.io"
+WWW="www"
+
+if [[ $1 ]]; then
+    SERVER="$1"
+fi
+
+echo "Server set to $SERVER"
+
+# Remove old files
+rm -r $WWW
+mkdir $WWW
+
+# Get index.html file
+wget "$SERVER/index.html" -O "$WWW/index.html"
+
+# Get the manifest file
+wget "$SERVER/manifest.appcache" -O "$WWW/manifest.appcache"
+
+# Read the manifest appcache
+ISCACHE=false
+
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    if [[ $line =~ [A-Z]+: ]]; then
+        if [[ $line == "CACHE:" ]]; then
+            ISCACHE=true
+        else
+            if [[ $ISCACHE == true ]]; then
+                ISCACHE=false
+
+                break
+            fi
+        fi
+
+        continue
+    fi
+
+    if [[ $ISCACHE == true && $line ]]; then
+        mkdir -p "$WWW/${line%/*}"
+
+        wget "$SERVER/$line" -O "$WWW/$line"
+    fi
+
+done < "$WWW/manifest.appcache"
